18ac4c0517552aeb5ea1f799e8656954b25784f1
yanlei-git <786584314@qq.com>
2019-10-23 15:20:30 +0800

    entity classes

---
+++ src/main/java/com/imooc/o2o/entity/Area.java
@@ -0,0 +1,52 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+public class Area {
+    // 这里使用Integer的原因是，基本类型默认赋值为0，而引用类型默认为null
+    private Integer areaId;
+    private String areaName;
+    private Integer priority;
+    private Date createTime;
+    private Date lastEditTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+}
