5c2da2b76a5e847fcc4f9b9530558c37c5e2f9a8
wz <296215200@qq.com>
2019-11-25 21:19:20 +0800

    积分功能DAO层开发

---
+++ src/main/java/com/imooc/o2o/dao/UserAwardMapDao.java
@@ -0,0 +1,71 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.entity.UserAwardMap;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @Author : Wang Zhen.
+ * @Date : Created in 18:13 2019/11/25
+ * @Description :
+ * @Modified By   :
+ * @Version :
+ */
+public interface UserAwardMapDao {
+    /**
+     * 根据传入进来的查询条件分页返回用户兑换奖品记录的列表信息
+     *
+     * @param userAwardCondition
+     * @param rowIndex
+     * @param pageSize
+     * @return
+     */
+    List<UserAwardMap> queryUserAwardMapList(@Param("userAwardCondition") UserAwardMap userAwardCondition,
+                                             @Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+    /**
+     * 配合queryUserAwardMapList返回相同查询条件下的兑换奖品记录数
+     *
+     * @param userAwardCondition
+     * @return
+     */
+    int queryUserAwardMapCount(@Param("userAwardCondition") UserAwardMap userAwardCondition);
+
+    /**
+     * 根据userAwardId返回某条奖品兑换信息
+     *
+     * @param userAwardId
+     * @return
+     */
+    UserAwardMap queryUserAwardMapById(long userAwardId);
+
+    /**
+     * 添加一条奖品兑换信息
+     *
+     * @param userAwardMap
+     * @return
+     */
+    int insertUserAwardMap(UserAwardMap userAwardMap);
+
+    /**
+     * 更新奖品兑换信息，主要更新奖品领取状态
+     *
+     * @param userAwardMap
+     * @return
+     */
+    int updateUserAwardMap(UserAwardMap userAwardMap);
+
+
+    /**
+     * 根据传入进来的查询条件分页返回用户兑换奖品记录的列表信息
+     *
+     * @param userAwardCondition
+     * @param rowIndex
+     * @param pageSize
+     * @return
+     */
+    List<UserAwardMap> queryReceivedUserAwardMapList(@Param("userAwardCondition") UserAwardMap userAwardCondition,
+                                                     @Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+}
--- src/test/java/com/imooc/o2o/dao/AreaDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/AreaDaoTest.java
@@ -2,13 +2,17 @@ package com.imooc.o2o.dao;
 
 import com.imooc.o2o.BaseTest;
 import com.imooc.o2o.entity.Area;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AreaDaoTest extends BaseTest {
     @Autowired
     private AreaDao areaDao;
